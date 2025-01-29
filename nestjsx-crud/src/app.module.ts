cad75ede2bfb51895ed807c58ef33ea4838bacd5
Yaroslav Makovskyi <makovskyiy@gmail.com>
2025-01-29 22:59:08 +0100

    feat: nestxjs-crud initial commit

---
+++ nestjsx-crud/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { User } from './users/entities/user.entity';
+import { Post } from './posts/entities/post.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, Post],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    PostsModule,
+  ],
+})
+export class AppModule {}
